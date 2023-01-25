5c02ce50f45f8076bffb5b9f755f2a3a8ccd411a
Jair Deyner <jairdeyner730791@gmail.com>
2023-01-25 09:10:18 -0500

    Login design

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginPageComponent {
+  showSpinner: boolean = false;
 
+  loginForm: FormGroup = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  isTheFieldInvalid(campo: string) {
+    return this.loginForm.controls[campo].errors && this.loginForm.controls[campo].dirty;
+  }
+
+  login() {}
 }
